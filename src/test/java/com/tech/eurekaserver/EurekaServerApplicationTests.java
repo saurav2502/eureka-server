51b56ba38dba48971ead2eb9d694d029833d11cb
Saurav Kumar <kumar_sauravsk@outlook.com>
2019-12-11 00:15:29 +0530

    Initial commit

---
+++ src/test/java/com/tech/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
